3b15750e43f5df67ac1116b4336dafa04b5c1aea
Martialis39 <sass@velvet.ee>
2020-04-16 06:25:34 +0300

    Create new instances of jumpPad, implement max number of jumpPads

--- scenes/jumpPad/JumpPad.gd
+++ scenes/jumpPad/JumpPad.gd
@@ -2,13 +2,12 @@ extends Node2D
 
 # Bounce Player up!! when player enters Area2D
 
-onready var indicator = $"TargetIndicator"
-onready var jump_pad = $"JumpPad"
+onready var indicator = $"TargetIndicator";
+onready var jump_pad = preload("res://scenes/JumpPad/IndividualJumpPad.tscn");
 onready var is_aiming = false;
-export (int) var placementSpeed = 5;
-
-func _ready():
-	jump_pad.hide();
+onready var list_of_jump_pads = [];
+onready var max_number_of_jump_pads = 2;
+export (int) var placement_speed = 5;
 
 func start_aiming():
 	Engine.time_scale = 0.1
@@ -20,22 +19,31 @@ func finish_aiming():
 	Engine.time_scale = 1
 	is_aiming = false;
 	indicator.hide();
-	jump_pad.position = indicator.position
-	jump_pad.show();
+	var jump_pad_instance = instantiate(jump_pad);
+	jump_pad_instance.position = indicator.position
+	list_of_jump_pads.append(jump_pad_instance)
+	if list_of_jump_pads.size() > max_number_of_jump_pads:
+		var first_jump_pad = list_of_jump_pads.pop_front();
+		first_jump_pad.queue_free()
 	pass
 
 func _physics_process(delta):
 	if is_aiming:
 		if Input.is_action_pressed("ui_right"):
-			indicator.position.x += placementSpeed
+			indicator.position.x += placement_speed
 		if Input.is_action_pressed("ui_left"):
-			indicator.position.x -= placementSpeed
+			indicator.position.x -= placement_speed
 		if Input.is_action_pressed("ui_up"):
-			indicator.position.y -= placementSpeed
+			indicator.position.y -= placement_speed
 		if Input.is_action_pressed("ui_down"):
-			indicator.position.y += placementSpeed
+			indicator.position.y += placement_speed
 
 func _on_Area2D_body_entered(body):
 	if body.has_method('bounce'):
 		body.queue_bounce_state()
 		pass
+
+func instantiate(preloadedScene):
+	var preloadedSceneInstance = preloadedScene.instance()
+	add_child(preloadedSceneInstance)
+	return preloadedSceneInstance
